3ff5ac966705d1b4f5e9ad7ee221fc3c2bf980f2
adityav31121999 <adityav31121999@zohomail.in>
2025-12-09 11:15:35 +0530

    version 0.0.3 in progress

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include "mononn.h"
+
+#define TRAIN_2D 1
+
+int main() {
+    try {
+        std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
+    }
+    catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Error getting CWD: " << e.what() << std::endl;
+    }
+
+    #ifdef __linux__
+        std::string path2Folder = "/home/adi23444/code/train";
+    #else
+        std::string path2Folder = "D:/train";
+    #endif
+
+    std::string digitTrain      =   path2Folder + "/digits_mnist/train";
+    std::string digitTest       =   path2Folder + "/digits_mnist/test";
+    std::string fashionTrain    =   path2Folder + "/fashion_mnist/train";
+    std::string fashionTest     =   path2Folder + "/fashion_mnist/test";
+    std::string progressData1   =   path2Folder + "/progress/progress1.csv";   // mnn digits
+    std::string progressData2   =   path2Folder + "/progress/progress2.csv";   // mnn fashion
+    std::string progressData3   =   path2Folder + "/progress/progress3.csv";   // mnn2d digits
+    std::string progressData4   =   path2Folder + "/progress/progress4.csv";   // mnn2d fashion
+    std::string testProgress1   =   path2Folder + "/progress/test_progress1.csv"; // mnn digits test
+    std::string testProgress2   =   path2Folder + "/progress/test_progress2.csv"; // mnn fashion test
+    std::string testProgress3   =   path2Folder + "/progress/test_progress3.csv"; // mnn2d digits test
+    std::string testProgress4   =   path2Folder + "/progress/test_progress4.csv"; // mnn2d fashion test
+    std::string binFileAddress1 =   path2Folder + "/weightsMNNdigits.bin";
+    std::string binFileAddress2 =   path2Folder + "/weightsMNNfashion.bin";
+    std::string binFileAddress3 =   path2Folder + "/weightsMNN2Ddigits.bin";
+    std::string binFileAddress4 =   path2Folder + "/weightsMNN2Dfashion.bin";
+
+    int inSize = 784;
+    int inh = 28, inw = 28;
+    int outSize = 10;
+    float order = 1.4f;
+    bool batchMode = 0;
+    bool useThreadOrBuffer = 0;
+    std::vector<int> hidden_layers1 = { 784, 392, outSize };
+    std::vector<int> hidden_layers2 = { 28, 56, 112, 112, 56, 28, outSize };
+
+    try {
+        std::cout << "THIS IS MONOMIAL NEURAL NETWORK IMPLEMENTATION" << std::endl;
+
+#if TRAIN_2D == 0
+        std::cout << "----------------------MNN----------------------" << std::endl;
+        mnn network1(inSize, outSize, hidden_layers1, order, binFileAddress1);
+        network1.loadNetwork();
+        network1.weightUpdateType = 3;
+        network1.path2progress = progressData1;
+        network1.path2test_progress = testProgress1;
+        network1.trainPrg.sessionSize = 50;
+        network1.onlineTraining(digitTrain, batchMode, useThreadOrBuffer);
+        // network1.miniBatchTraining(digitTrain, batchMode);
+        // network1.fullDataSetTraining(digitTrain, useThreadOrBuffer);
+        // network1.test(digitTest, useThreadOrBuffer);
+#else
+        std::cout << "---------------------MNN2D---------------------" << std::endl;
+        mnn2d network2(inh, inw, outSize, hidden_layers2, order, binFileAddress3);
+        network2.initiateWeights(3);
+        network2.weightUpdateType = 3;
+        network2.path2progress = progressData3;
+        network2.path2test_progress = testProgress3;
+        network2.trainPrg.sessionSize = 50;
+        network2.onlineTraining(digitTrain, batchMode, useThreadOrBuffer);
+        // network2.miniBatchTraining(digitTrain, batchMode);
+        // network2.fullDataSetTraining(digitTrain, useThreadOrBuffer);
+        // network2.test(digitTest, useThreadOrBuffer);
+#endif
+    }
+    catch (const std::exception& e) {
+        std::cerr << "An exception occurred in main: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
